772667580c2dbcc02d3f46fb4a76a18e11fa42b1
emin <emin.roblack@gmail.com>
2021-05-30 17:37:41 +0200

    Initial Commit

---
+++ logostretch/logostretchApp.swift
@@ -0,0 +1,17 @@
+//
+//  logostretchApp.swift
+//  logostretch
+//
+//  Created by Emin Grbo on 30/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct logostretchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
